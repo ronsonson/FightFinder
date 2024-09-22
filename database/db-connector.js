79db9b90b425d3f120b6f2ac6de7930c0f3da95f
ronsonson <147671329+ronsonson@users.noreply.github.com>
2024-09-22 12:14:26 -0700

    removing host info
    removing host info in order to make repo public

--- database/db-connector.js
+++ database/db-connector.js
@@ -6,10 +6,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_imfeldr',
-    password        : '2039',
-    database        : 'cs340_imfeldr'
+    host            : '',
+    user            : '',
+    password        : '',
+    database        : ''
 })
 
 // Export it for use in our applicaiton
